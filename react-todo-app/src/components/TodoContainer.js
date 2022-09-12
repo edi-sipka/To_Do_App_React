d5b06181ead0ca75743c1ab0c2a8de2d173b4d67
edi-sipka@gmail.com <edisipka@gmail.com>
2022-09-12 15:41:06 +0100

    Add storage

--- react-todo-app/src/components/TodoContainer.js
+++ react-todo-app/src/components/TodoContainer.js
@@ -6,23 +6,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: 'Setup development environment',
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: 'Develop website and add content',
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: 'Deploy to live server',
-        completed: false,
-      },
-    ],
+    todos: [],
   };
 
   handleChange = (id) => {
@@ -68,6 +52,23 @@ class TodoContainer extends React.Component {
       }),
     });
   };
+  componentDidMount() {
+    const temp = localStorage.getItem('todos');
+    const loadedTodos = JSON.parse(temp);
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos);
+      localStorage.setItem('todos', temp);
+    }
+  }
+
   render() {
     return (
       <div className="container">
