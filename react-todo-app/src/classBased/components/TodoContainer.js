51110e0a85ad04c5ecdd20c6b4cbf95d42673791
edi-sipka@gmail.com <edisipka@gmail.com>
2022-09-13 10:43:27 +0100

    Fix linters

--- react-todo-app/src/classBased/components/TodoContainer.js
+++ react-todo-app/src/classBased/components/TodoContainer.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import TodosList from './TodosList';
 import Header from './Header';
--- react-todo-app/src/functionBased/components/TodoItem.js
+++ react-todo-app/src/functionBased/components/TodoItem.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import styles from './TodoItem.module.css';
 
